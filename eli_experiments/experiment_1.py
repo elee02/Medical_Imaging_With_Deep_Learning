0e87c180cfd1e2bd6abecd412039c6f9d47fbd85
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-11-04 14:27:56 +0900

    update

--- eli_experiments/experiment_1.py
+++ eli_experiments/experiment_1.py
@@ -1,4 +1,4 @@
 '''Setting the seed for reproducibility '''
 import torch
 torch.manual_seed(0)
-print(torch.rand(2)) # Run twice to see the same random numbers
+print(torch.rand()) # Run twice to see the same random numbers
