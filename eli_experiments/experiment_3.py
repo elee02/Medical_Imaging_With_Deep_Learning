fd1b212df3b27dfb05832f2599dd683eeea93815
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-11-04 15:08:39 +0900

    Add multiple assignment example using .size() member

--- eli_experiments/experiment_3.py
+++ eli_experiments/experiment_3.py
@@ -5,3 +5,13 @@ x = torch.rand([5,11,3,2])
 print(x.shape) # overall shape
 
 print(x.size(2)) # size of the dimension at the index 2 (third dimesion)
+
+print()
+# Multiple assignment with .size() member
+a, b, c, d = x.size()
+print(f'Multuple assinment:\
+\n      a, b, c, d = x.size(): \
+\n      a: {a}\
+\n      b: {b}\
+\n      c: {c}\
+\n      d: {d}')
