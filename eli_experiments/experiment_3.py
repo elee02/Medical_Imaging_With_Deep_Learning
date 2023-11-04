294a996a1b4c98bed7081a738f069d46023c7a94
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-11-04 14:57:37 +0900

    Fix print statements and add comments to experiment_3.py

--- eli_experiments/experiment_3.py
+++ eli_experiments/experiment_3.py
@@ -2,6 +2,6 @@
 import torch
 
 x = torch.rand([5,11,3,2])
-print(x.shape)
+print(x.shape) # overall shape
 
-print(x.size(2))
+print(x.size(2)) # size of the dimension at the index 2 (third dimesion)
