0953c900fda387e899441803e245c6109c6f042b
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-11-05 13:15:29 +0900

    Fixing random number generation in experiment_1.py

--- eli_experiments/experiment_1.py
+++ eli_experiments/experiment_1.py
@@ -1,4 +1,4 @@
 '''Setting the seed for reproducibility '''
 import torch
 torch.manual_seed(0)
-print(torch.rand()) # Run twice to see the same random numbers
+print(torch.rand(1)) # Run twice to see the same random numbers
