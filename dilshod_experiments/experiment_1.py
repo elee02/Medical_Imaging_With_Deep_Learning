79e441eaefa9ee4f43fbca7283c012788934a6e5
Dilshod <dilshod.profile@gmail.com>
2023-11-05 11:51:33 +0900

    experiment-1

--- dilshod_experiments/experiment_1.py
+++ dilshod_experiments/experiment_1.py
@@ -1,2 +1,4 @@
-'''Hello_World!'''
-print("Hello, world!")
+''' Setting seed to get reproducible random numbers '''
+import torch
+torch.manual_seed(42)  # Setting the seed
+print(torch.rand(2,3)) # Run twice to get the same random numbers
