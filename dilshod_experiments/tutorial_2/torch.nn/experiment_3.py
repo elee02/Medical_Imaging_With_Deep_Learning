d6f4057774a601242262aa62fc0c134326c73915
Dilshod <dilshod.profile@gmail.com>
2023-11-09 14:37:05 +0900

    The Data

---
+++ dilshod_experiments/tutorial_2/torch.nn/experiment_3.py
@@ -0,0 +1,11 @@
+''' The Data '''
+
+import torch.utils.data as data
+# The data package defines two classes which are 
+# the standard interface for handling data in PyTorch
+
+# data.Dataset:
+# provides an uniform interface to access the training/test data
+
+# data.DataLoader:
+# efficiently load and stack the data points from the dataset into batches during training
