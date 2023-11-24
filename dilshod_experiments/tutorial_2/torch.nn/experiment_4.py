f9fc5df01b01435a1253df16cc0e53dfbcf0f016
Dilshod <dilshod.profile@gmail.com>
2023-11-24 18:33:15 +0900

    The Dataset Class

---
+++ dilshod_experiments/tutorial_2/torch.nn/experiment_4.py
@@ -0,0 +1,8 @@
+""" The Dataset Class """
+
+# The dataset class summarizes the basic functionality of a dataset
+
+
+# The getitem function returns the  i-th data point in the dataset
+
+# The len function returns the size of the dataset
